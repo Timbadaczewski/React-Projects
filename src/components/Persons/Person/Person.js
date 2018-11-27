c095dfa8318118f77c03148fdd8b59487b3f597d
Tim Badaczewski <tibadaczewski@rbh12552.redbrickhealth.local>
2018-11-26 20:21:04 -0600

    found capital letter causing input issue

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -7,7 +7,7 @@ const person = (props)=> {
         <div className={classes.Person}>
             <p onClick={props.click}>I'm {props.name} and I am {props.age} years old</p>
             <p>{props.children}</p>
-            <input type="text" onChange={props.changed} value={props.value}/>
+            <input type="text" onChange={props.changed} value={props.name}/>
 
         </div>
     )
